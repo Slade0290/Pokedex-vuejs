1d257f3bfa2a193b75b72d74526b200f3083f1f4
Slade0290 <ly.david@live.fr>
2022-05-18 18:52:20 +0200

    Fonctionality done. Missing design.

--- src/components/PokedexPage.vue
+++ src/components/PokedexPage.vue
@@ -1,42 +1,73 @@
 <template>
-  <div>
-    Hello
+  <div class="main">
+    <h1>Pokedex</h1>
+    <div v-for="pokemon in pokemonList.results" v-bind:key="pokemon">
+      <div class="pname" v-on:click="getPokemon(pokemon.url)">
+        {{pokemon.name}}
+      </div>
+    </div>
+    <PokemonModal v-if="this.showModal" :pokemon="this.pokemon" @closeModal="updateModalState"/>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import PokemonModal from './PokemonModal'
 
 export default {
   name: 'PokedexPage',
+  components: {
+    PokemonModal
+  },
   created: function() {
       document.title = 'Pokedex encyclopedia'
   },
   mounted() {
-    this.getPokemon(100)
+    this.getPokemonList(0, 10)
   },
   data() {
     return {
-      posts: [],
+      pokemonList: [],
+      pokemon: {},
+      showModal: false,
     }
   },
   methods: {
-    async getPokemon(number) {
+    async getPokemonList(start, number) {
       try {
         await axios.get(
-          `https://pokeapi.co/api/v2/pokemon?limit=${number}&offset=0`
+          `https://pokeapi.co/api/v2/pokemon?limit=${number}&offset=${start}`
         ).then(res => {
-          this.posts = res.data
-          console.log(this.posts)
+          this.pokemonList = res.data
         })
-      } catch (error) {
-        console.log(error);
+      } catch (e) {
+        console.error(e)
       }
     },
+    async getPokemon(url) {
+      try {
+        await axios.get(url).then(res => {
+          this.pokemon = res.data
+          console.log(this.pokemon)
+        })
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    updateModalState(val) {
+      this.showModal = val
+    }
   },
+  watch: {
+    pokemon() {
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.pname {
+  cursor: pointer;
+}
 </style>
---
+++ src/components/PokemonModal.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">  
+      <span class="close" v-on:click="this.closeModal()">&times;</span>
+      <div class="sub-content">
+        <h1 class="pname">{{pokemon.name}}</h1>
+        <p>Name: {{pokemon.name}}</p>
+        <p>Name: {{pokemon.name}}</p>
+        <p>Name: {{pokemon.name}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokemonModal',
+  props: {
+    pokemon: {}
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  mounted() {
+    this.initPageInfo()
+  },
+  methods: {
+    initPageInfo() {
+      document.title = `Pokedex encyclopedia - ${this.pokemon.name}`
+    },
+    closeModal() {
+      this.$emit('closeModal', this.showModal)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .modal {
+    position: fixed; 
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0,0,0,0.4);
+    .modal-content {
+      background-color: #fefefe;
+      border-radius: 1rem;
+      margin: 15% auto; 
+      padding: 20px;
+      border: 1px solid #888;
+      box-shadow: .2rem .2rem .5rem .1rem rgba(25,25,25,0.8);
+      width: 80%;
+      .close {
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+        transition: .5s;
+        &:hover,
+        &:focus {
+          color: black;
+          text-decoration: none;
+          cursor: pointer;
+        }
+      }
+      .sub-content {
+        margin: 3rem;
+        .pname {
+          text-transform: capitalize;
+        }
+      }
+    }
+  }
+</style>
